45cc9f939ca1284e18be630bb92c548bd9124aef
olimaol <oli_maith@gmx.de>
2024-03-15 08:33:43 +0100

    restored trial_BG

--- run_parallel.py
+++
@@ -1,9 +0,0 @@
-from CompNeuroPy import run_script_parallel
-
-if __name__ == "__main__":
-    args_list = [[f"{sim_id}", "0"] for sim_id in range(1, 61)]
-    run_script_parallel(
-        script_path="run_cla_Training.py",
-        n_jobs=20,
-        args_list=args_list,
-    )
